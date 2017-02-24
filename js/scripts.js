a11bc3aa43aa89df71b769c05084cb1ee752aad6
Mayieko <michaelmayieko@gmail.com>
2017-02-24 15:46:18 +0300

    pingpong with js

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// Front End Logic
+$(document).ready(function(){
+  $("#pingPong").submit(function(event){
+    var userInput = parseInt($("#userInput").val());
+    var arrayNum = pingPong(userInput);
+    $("#result").text(arrayNum);
+    event.preventDefault();
+
+
+  });
+});
+
+// Business Logic
+
+function pingPong(userInput) {
+  debugger;
+  var arrayPingPong = [];
+  for (var i = 1; i < userInput + 1; i++) {
+    if (i % 3 === 0) {
+      arrayPingPong.push("ping");
+
+    } else if (i % 5 === 0) {
+      arrayPingPong.push("pong");
+
+    } else if (i % 5 === 0 && i % 3 === 0) {
+      arrayPingPong.push("pingpong");
+    } else {
+      arrayPingPong.push(i);
+    }
+
+  }
+  return arrayPingPong;
+
+};
